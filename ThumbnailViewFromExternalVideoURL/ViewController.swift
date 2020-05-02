bf3f4aecad0576c06b5df254a9563cac3444e702
Shanmugarajeshwaran R <shanmugarajeshwaranr@gmail.com>
2020-05-02 16:12:49 +0530

    Initial Commit

---
+++ ThumbnailViewFromExternalVideoURL/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThumbnailViewFromExternalVideoURL
+//
+//  Created by ADMIN on 02/05/20.
+//  Copyright © 2020 Success Resource Pte Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
